d4aec62271b385df67e8f14a4d1f3965add2f774
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-06-25 18:03:24 -0700

    Update 26_remove_duplicates_from_sorted_array.py

--- 26_remove_duplicates_from_sorted_array.py
+++ 26_remove_duplicates_from_sorted_array.py
@@ -1,3 +1,16 @@
+# 26 Remove Duplicates from Sorted Array
+
+"""
+Solution 1 (implementation using while loop)
+Runtime: 129 ms (beats 16.45%)
+Memory: 14.6 MB (beats 64.18%)
+
+Intuition and Approach
+- Set a pointer (i). Iterate through the nums array/list. 
+- For each iteration compare integer in nums at index [i] with the integer at index [i+1].
+- If integer at index [i] equals the integer at index [i-1], pop integer at index [i+1]; otherwise increment i
+- Return the post-modification length of nums
+"""
 class Solution(object):
     def removeDuplicates(self, nums):
         """
@@ -5,10 +18,21 @@ class Solution(object):
         :rtype: int
         """
         i = 0
-        # Implementation with while loop
         while i < len(nums) - 1:
             if nums[i] == nums[i+1]:
                 nums.pop(i+1)
             else:
                 i += 1
         return len(nums)
+
+"""
+Solution 2 (implementation using for loop)
+Runtime: 129 ms (beats 16.45%)
+Memory: 14.6 MB (beats 64.18%)
+
+Intuition and Approach
+- Set a pointer (i). Iterate through the nums array/list. 
+- For each iteration compare integer in nums at index [i] with the integer at index [i+1].
+- If integer at index [i] equals the integer at index [i-1], pop integer at index [i+1]; otherwise increment i
+- Return the post-modification length of nums
+"""
