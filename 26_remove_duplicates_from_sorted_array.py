40a316f3cd267922f07f962428403d6fa19e0bb2
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-06-25 19:05:19 -0700

    Update 26_remove_duplicates_from_sorted_array.py

--- 26_remove_duplicates_from_sorted_array.py
+++ 26_remove_duplicates_from_sorted_array.py
@@ -26,13 +26,24 @@ class Solution(object):
         return len(nums)
 
 """
-Solution 2 (implementation using for loop)
-Runtime: 129 ms (beats 16.45%)
-Memory: 14.6 MB (beats 64.18%)
+Solution 2 (for loop + two pointers in use)
+Runtime: 50 ms (beats 99.90%)
+Memory: 15 MB (beats 8.12%)
 
 Intuition and Approach
-- Set a pointer (i). Iterate through the nums array/list. 
-- For each iteration compare integer in nums at index [i] with the integer at index [i+1].
-- If integer at index [i] equals the integer at index [i-1], pop integer at index [i+1]; otherwise increment i
-- Return the post-modification length of nums
+- Set two pointers i and j. j will keep track the number of unique elements encountered thus far and i will trace the list/array in range(1, len(nums))
+- j will point to the first instance of a unique element. i will continuing looping until it finds the very next unique element
+- The next unique element, once found, will be placed following the last unique element.
+- j will point to the newly situated/displaced unique element.
+- This process of updating and incrementing will continue until i has reached the last element and j is pointing to the final unique element (alternatively, 
+when j is pointing to the largest unique element in the list).
 """
+
+class Solution(object):
+	def removeDuplicates(self, nums):
+		j = 0
+		for i in range(1, len(nums)):
+			if nums[j] != nums[i]:
+				j += 1
+				nums[j] = nums[i]
+		return j + 1
