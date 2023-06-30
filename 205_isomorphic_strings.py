a2a4e7dd63da334675695d196f091139401af87a
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-06-29 20:49:53 -0700

    Update 205_isomorphic_strings.py

--- 205_isomorphic_strings.py
+++ 205_isomorphic_strings.py
@@ -14,9 +14,6 @@ Inution and Approach
 - Return True, if the condition of falsehood is not met
 
 """
-
-
-
 class Solution(object):
     def isIsomorphic(self, s, t):
         """
@@ -32,5 +29,43 @@ class Solution(object):
                 iso_dict[s[i]] = t[i]
             elif t[i] != iso_dict.get(s[i]):
                 return False
+        
+        return True
+
+
+"""
+Solution 2 (Bidirectional Mapping Check) 
+
+Time Complexity: O(n)
+Space Complexity:O(n)
+
+Inution and Approach
+- Create two dictionary with the key-value pairs corresponding to the characters in s and t, In one dictionary the charcters in s and t are the keys and values, respectively. For the second dictionary, characters in t are the key and characters in s will be values
+- Iterate over the strings s and t, character by character. 
+- If the key-values pairs in BOTH dictionaries are not one-to-one, return False.
+- Otherwise, add new entries to the dictionary
+- Return True, if the falsehood condition is not met through all iterations
+
+"""
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        st_dict, ts_dict = {}, {}
+
+        for i in range(len(s)):
+            char1, char2 = s[i], t[i]
+
+            if ((char1 in st_dict and char2 != st_dict[char1]) or
+            (char2 in ts_dict and char1 != ts_dict[char2])):
+                return False
+            
+            st_dict[char1] = char2
+            ts_dict[char2] = char1
+
         
         return True
