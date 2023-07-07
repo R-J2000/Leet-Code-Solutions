8e3374bc3455bbe0b65a1a67502859ad468199d4
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-07-06 23:35:41 -0700

    Update 274_h_index.py

--- 274_h_index.py
+++ 274_h_index.py
@@ -34,3 +34,31 @@ class Solution(object):
                 h_index -= 1
         
         return 0
+
+"""
+Solution 2 (Sorting)
+
+Time Complexity: O(nlog(n))
+Space Complexity: O(1)
+
+Intuition and Approach
+- Sort citations
+- Compare citations to its index. If index >= citations return index (h papers that have each been cited at least h times.)
+- Otherwise, return len(citations). A scenario where citations are always greater than the index. He the h-index will be bound not by the number of citations, but by the number of papers
+
+
+"""
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+
+        citations.sort(reverse = True)
+
+        ind = 0
+        for ind, citation in enumerate(citations):
+            if ind >= citation:
+                return ind
+        return len(citations)
