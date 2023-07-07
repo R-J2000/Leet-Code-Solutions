6d2cef0a97eb0b12b4f78d44659d4fc87e4b414e
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-07-06 23:02:02 -0700

    Create 274_h_index.py

---
+++ 274_h_index.py
@@ -0,0 +1,36 @@
+# 274 H-Index
+
+"""
+Solution 1 (Bad Time Complexity)
+
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+
+Intuition and Approach
+- Set the h-index to the total number of citations. We will try for the max possible h-index val and decrement iteratively until we get to 0. In which case return 0.
+- Iterate over the citations. If a particular citation is above the current h-index, update counter.
+- After iteration, check if counter >= h-index. If yes, this means at least h papers have been cited h times.
+- If not, decrement h-index and try for a smaller h-index value
+
+"""
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+
+        h_index = len(citations)
+
+        while h_index != 0:
+            counter = 0
+            for i in citations:
+                if i >= h_index:
+                    counter += 1
+                
+            if counter >= h_index:
+                return h_index
+            else:
+                h_index -= 1
+        
+        return 0
