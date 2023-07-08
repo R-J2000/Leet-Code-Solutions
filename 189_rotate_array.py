ac75c39d7e6ee9c366709c46ef182b06f9f1006d
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-07-08 16:43:00 -0700

    Update 189_rotate_array.py

--- 189_rotate_array.py
+++ 189_rotate_array.py
@@ -10,29 +10,26 @@ Intuition and Approach
 - Define helper function that allows you to reverse a sub-array from defined indicies
 - Update k to minimize the number of rotations. A k value of 11 and 21 for an array of length 10, will have the same 
 end result. Do this by exploiting the modulus operator
-- Reverse the first len(nums) - k of nums
-- Reverse the last k element of nums
 - Reverse nums
+- Reverse the first k element of nums 
+- Reverse the last len(nums) - k of nums
 """
-class Solution(object):
-    def reverse (self, nums, left_ind, right_ind) : 
-
-        while left_ind < right_ind:
-            temp = nums[left_ind]
-            nums[left_ind] = nums[right_ind]
-            nums[right_ind] = temp
-            
-            left_ind += 1
-            right_ind -= 1
-
+class Solution(object):         
     def rotate(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
+        def reverse(nums, start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        n = len(nums)
+        k = k % n  
         
-        self.reverse(nums, 0, len(nums) - k - 1);
-        self.reverse(nums, len(nums) - k, len(nums) - 1);
-        self.reverse(nums, 0, len(nums) - 1);
+        reverse(nums, 0, n - 1)  
+        reverse(nums, 0, k - 1)  
+        reverse(nums, k, n - 1)
