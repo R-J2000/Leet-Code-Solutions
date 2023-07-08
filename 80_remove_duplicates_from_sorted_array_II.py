81bb693a9c4375b6e94454a736bcb5e3cae05f42
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-07-08 15:24:24 -0700

    Update 80_remove_duplicates_from_sorted_array_II.py

--- 80_remove_duplicates_from_sorted_array_II.py
+++ 80_remove_duplicates_from_sorted_array_II.py
@@ -7,10 +7,16 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 
 Intuition and Approach
-
-    TODO 
+- Account of the edge case were nums only has 2 or fewer elements
+- Initialize tracker variable
+- Interate nums from index 2 onwards. 
+- Any time nums[tracker] != nums[i], (i.e we come across a non-duplicate entry) modify the list, 
+by overwriting the value at the index tracker with the non-duplicate value. Increment tracker by 1.
+- In the end, return tracker
 
 Special Notes: 
+Realize that anytime nums[i] == nums[tracker] implies a value with more than 2 duplicates. tracker can be used to keep track of this position. Once we come across
+a distinct value, the overwriting process can begin.
 """
 
 class Solution(object):
@@ -24,9 +30,9 @@ class Solution(object):
             return len(nums)
     
 		
-        tracker = 0  # Pointer from where we need to replace elements
+        tracker = 2 
         for i in range(2, len(nums)):
-            if nums[i] != nums[tracker]:
-                nums[tracker + 2] = nums[i]
+            if nums[i] != nums[tracker - 2]:
+                nums[tracker] = nums[i]
                 tracker += 1
-        return tracker + 2
+        return tracker
