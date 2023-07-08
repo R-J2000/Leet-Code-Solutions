080b6cd925c68852c4487e4a435c286231e3cff8
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-07-08 15:43:02 -0700

    Update 55_jump_game.py

--- 55_jump_game.py
+++ 55_jump_game.py
@@ -7,7 +7,11 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 
 Intuition and Approach
-TODO
+- Our goal is to reach the last element of nums. Initialize a variable goal to the last index.
+- Interate through nums from right to left (back to front). 
+- If we can "jump" from index i to goal, we reset goal = i. The intution being that getting to i will ensure that we can get to the last index, hence 
+it can be our new goal
+- If goal == 0, then return True. This means their is a path from index 0 to the last index. Otherwise, return false.
 """
 class Solution(object):
     def canJump(self, nums):
