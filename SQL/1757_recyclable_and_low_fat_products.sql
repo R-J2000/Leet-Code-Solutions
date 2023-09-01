9b9d27af640f2498cbe2dc185637598ffeadbbf1
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-09-01 15:08:22 -0700

    Update 1757_recyclable_and_low_fat_products.sql

--- SQL/1757_recyclable_and_low_fat_products.sql
+++ SQL/1757_recyclable_and_low_fat_products.sql
@@ -2,8 +2,8 @@
 /*
 Intuition and Approach
 - Use the SELECT and FROM key words to retrieve the product_id column from Products Table
-- Use the WHERE to filter rows, ensuring only those where the specified conditions (the product
-    is both low fat and recyclable) are met.
+- Use the WHERE to filter rows, ensuring only those rows where the specified conditions are met (the product
+    is both low fat and recyclable) remain in the retrieved column
 */
 
 SELECT product_id 
