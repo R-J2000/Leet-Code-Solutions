c11966d83c6d393da77605cbb28045dffe3ad974
R-J2000 <136933973+R-J2000@users.noreply.github.com>
2023-09-07 17:01:27 -0700

    Update 577_employee_bonus.sql

--- SQL/577_employee_bonus.sql
+++ SQL/577_employee_bonus.sql
@@ -1,7 +1,7 @@
 # 577 Employee Bonus
 
 /*
-Solution 1 (Most Intuitive)
+Solution 1 a. (Most Intuitive)
 Intuition and Approach
 - Use the SELECT and FROM key words to retrieve the name and bonus fields from their respective tables
 - RIGHT JOIN the two table for rows where Employee.empID = Bonus.empID is satisfied and ALL rows of the name column are preserved.
@@ -12,3 +12,13 @@ SELECT Employee.name, Bonus.bonus
 FROM Bonus
 RIGHT JOIN Employee ON Employee.empID = Bonus.empID
 WHERE IFNULL(Bonus.bonus, 0) < 1000
+
+
+/*
+Solution 1 b. (Same approach; different syntax)
+*/
+
+SELECT E.name, B.bonus
+FROM Bonus B
+RIGHT JOIN Employee E USING(empID)
+WHERE IFNULL(B.bonus, 0) < 1000
